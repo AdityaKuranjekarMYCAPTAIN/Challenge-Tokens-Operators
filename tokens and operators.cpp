363f27ee844cb4d1389add201062947703efe12f
Aditya Kuranjekar <81797057+AdityaKuranjekarMYCAPTAIN@users.noreply.github.com>
2021-05-03 22:02:08 +0530

    Create tokens and operators.cpp

---
+++ tokens and operators.cpp
@@ -0,0 +1,30 @@
+//1. To find size of int, float, double and char in your system.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Size of char: "<<sizeof(char)<<" byte\n";
+	cout<<"Size of char: "<<sizeof(int)<<" bytes\n";
+	cout<<"Size of char: "<<sizeof(float)<<" bytes\n";
+	cout<<"Size of char: "<<sizeof(double)<<" bytes\n";
+	return 0;
+}
+
+//2.To find product of two numbers entered by user
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float a , b ,c;
+	cout << "Enter two numbers: ";
+	cin >> a;
+	cin >> b;
+	c = a*b;
+	cout << "\nProduct = "<<c;
+	return 0;
+}
+
+//Please commnet out one program before running the other
+//by.....Aditya Kuranjekar under Captain Aadhar..-->HAPPY CODING
